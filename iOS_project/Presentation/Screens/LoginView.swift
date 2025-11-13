cf33054dd16509042668531cf2f443575138a808
j02on <pjylove08@naver.com>
2025-11-13 13:30:29 +0900

    feat (#3) : 회원가입 nav 추가

--- iOS_project/Presentation/Screens/LoginView.swift
+++ iOS_project/Presentation/Screens/LoginView.swift
@@ -9,7 +9,7 @@ struct LoginView: View {
             VStack (alignment: .leading, spacing: 16) {
                 Text("로그인")
                     .font(.system(size: 36, weight: .bold))
-                Text("로그인 후 Tripick을 이용해보세요")
+                Text("로그인 후 서비스를 이용해보세요")
                     .font(.system(size: 20, weight: .regular))
                     .foregroundColor(Colors.Gray.g600)
             }
@@ -17,7 +17,14 @@ struct LoginView: View {
                 InputField(label: "아이디", text: $id, placeholder: "아이디를 입력하세요" )
                 InputField(label: "비밀번호", text: $password, placeholder: "비밀번호를 입력하세요", isPwd: true )
             }
-            ButtonField(children: "로그인")
+            VStack(spacing: 12) {
+                ButtonField(children: "로그인")
+                NavigationLink(destination: LoginView()) {
+                    Text("회원가입")
+                        .font(.system(size: 16, weight: .regular))
+                        .foregroundColor(Color(Colors.Gray.g600))
+                }
+             }
         }
         .padding()
     }
