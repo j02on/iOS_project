99f264d66384803c3096ccfeb8cb8c7bc299b0ca
j02on <pjylove08@naver.com>
2025-11-13 10:21:17 +0900

    setting (#1) : setting

---
+++ iOS_project/iOS_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_projectApp.swift
+//  iOS_project
+//
+//  Created by 🩷 on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS_projectTests/iOS_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_projectTests.swift
+//  iOS_projectTests
+//
+//  Created by 🩷 on 11/12/25.
+//
+
+import Testing
+@testable import iOS_project
+
+struct iOS_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
